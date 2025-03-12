3359126d40acb6bbd9f75a5510b79da8aabd291a
mauromotion <hello@mauromotion.com>
2025-03-12 12:00:06 +0000

    set up robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import robotsTxt from 'astro-robots-txt';
+import { robotsConfig } from './robots.config';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mauromotion.com',
+
   markdown: {
     shikiConfig: {
       theme: 'poimandres',
     },
   },
+
+  integrations: [robotsTxt(robotsConfig)],
 });
+
